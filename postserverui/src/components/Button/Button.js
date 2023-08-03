5bdc920cde0787ae7f306486099c7a001a2dd76d
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-08-03 13:51:40 +0100

    ok working now

---
+++ postserverui/src/components/Button/Button.js
@@ -0,0 +1,14 @@
+
+const Button = (props) => {
+    // eslint-disable-next-line react/prop-types
+    const { variant = 'primary', children, ...rest } = props
+    return (
+        <div>
+            <button className={`button ${variant}`} {...rest}>
+                {children}
+            </button>
+        </div>
+    );
+}
+
+export default Button;
