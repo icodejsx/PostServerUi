21e827d9b690b7d0f58deb0136fe754e1a37d01d
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-08-10 22:55:36 +0100

    working

--- postserverui/src/components/HomePage.jsx
+++ postserverui/src/components/HomePage.jsx
@@ -6,6 +6,12 @@ const HomePage = () => {
     return (
         <div>
             <Navbar />
+            <div className='flex w-full justify-center items-center mt-12'>
+                <img className=' w-[600px]' src='home.svg' alt='home' />
+                <form>
+
+                </form>
+            </div>
 
         </div>
     );
--- postserverui/src/components/Navbar/Navbar.jsx
+++ postserverui/src/components/Navbar/Navbar.jsx
@@ -3,8 +3,21 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='flex p-4 justify-around items-center border border-4'>
+            <div className='flex items items-end gap-1 '>
+                <p className='text-4xl font-bold color'>My</p>
+                <p className='font-bold'>Post</p>
+                <p className='text-2xl font-bold'>App</p>
+            </div>
+            <div>
 
+
+                <p>Posts</p>
+            </div>
+
+            <button>
+                Login
+            </button>
         </div>
     );
 }
